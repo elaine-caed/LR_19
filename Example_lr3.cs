5085fc196b669767c5c3cf5981020c8a95912ae0
elaine-caed <77505253+elaine-caed@users.noreply.github.com>
2021-05-05 23:15:48 +0300

    Add files via upload

---
+++ Example_lr3.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Example
+{
+    class Program
+    {
+        static double Function(double x)
+        {
+            return x * x;
+        }
+        static void Main(string[] args)
+        {
+        StartOfCalculations:
+            Console.Write("Введiть початок вiдрiзку iнтегрування a: ");
+            string sa = Console.ReadLine();
+            double a = double.Parse(sa);
+            Console.Write("Введiть кiнець вiдрiзку iнтегрування b: ");
+            string sb = Console.ReadLine();
+            double b = double.Parse(sb);
+            Console.Write("Введiть кiлькiсть дiлянок n: ");
+            string sn = Console.ReadLine();
+            double n = double.Parse(sn);
+            double dx = (b - a) / n;
+            double y1, y2;
+            double x1, x2;
+            double Intgrl = 0;
+            for (int i = 0; i < n; i++)
+            {
+                x1 = a + i * dx;
+                x2 = x1 + dx;
+                y1 = Function(x1);
+                y2 = Function(x2);
+                Intgrl += (y1 + y2) / 2 * dx;
+            }
+            Console.WriteLine(
+        "Iнтеграл функцiї на вiдрiзку [{0}, {1}] становить {2:0.0000}", a, b, Intgrl);
+            Console.Write("Повторити розрахунок (y - так) ? ");
+            ConsoleKeyInfo pressedKey = Console.ReadKey();
+            Console.WriteLine();
+            if (pressedKey.Key == ConsoleKey.Y)
+            {
+                Console.WriteLine();
+                goto StartOfCalculations;
+            }
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _19_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(textBox1.Text);
+            double b = Convert.ToDouble(textBox2.Text);
+            double n = 0.1;
+            double x1, x2;
+            double x, y, h;
+            double Intgrl=0;
+            h = (b - a) / n;
+            x = a;
+            this.chart1.Series[0].Points.Clear();
+            for (int i = 0; i < n; i++)
+            {
+                x1 = 2 * x;
+                x2 = 5 * x;
+                y = 45 * x1 * Math.Cos(x2) + Math.Pow(9 * x2 * Math.Pow(x1, 3), 0.5);
+                x = i + h;
+                Intgrl += y / h;
+                this.chart1.Series[0].Points.AddXY(x, y);
+            }
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+            chart1.ChartAreas[0].AxisX.ScaleView.Zoom(0, 50);
+            chart1.ChartAreas[0].CursorX.IsUserEnabled = true;
+            chart1.ChartAreas[0].CursorX.IsUserSelectionEnabled = true;
+            chart1.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
+            chart1.ChartAreas[0].AxisX.ScrollBar.IsPositionedInside = true;
+
+            chart1.ChartAreas[0].AxisY.ScaleView.Zoom(-1, 1);
+            chart1.ChartAreas[0].CursorY.IsUserEnabled = true;
+            chart1.ChartAreas[0].CursorY.IsUserSelectionEnabled = true;
+            chart1.ChartAreas[0].AxisY.ScaleView.Zoomable = true;
+            chart1.ChartAreas[0].AxisY.ScrollBar.IsPositionedInside = true;
+        }
+    }
+}
